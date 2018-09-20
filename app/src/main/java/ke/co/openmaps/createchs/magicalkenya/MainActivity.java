9c9e975d885d293cd92d80e409bac1e0a3a89b31
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-09-20 20:36:40 +0300

    add navigation drawer

--- app/src/main/java/ke/co/openmaps/createchs/magicalkenya/MainActivity.java
+++ app/src/main/java/ke/co/openmaps/createchs/magicalkenya/MainActivity.java
@@ -1,7 +1,12 @@
 package ke.co.openmaps.createchs.magicalkenya;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                item.setChecked(true);
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.content, new PlacesFragment())
+                .commit();
     }
 }
