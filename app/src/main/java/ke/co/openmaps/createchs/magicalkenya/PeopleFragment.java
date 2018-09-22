3c7681af88f49999acc679962d31ed0fa33b4728
Ngari Ndung'u <ngari.ndungu@gmail.com>
2018-09-22 12:57:53 +0300

    add remaining data

--- app/src/main/java/ke/co/openmaps/createchs/magicalkenya/PeopleFragment.java
+++ app/src/main/java/ke/co/openmaps/createchs/magicalkenya/PeopleFragment.java
@@ -6,13 +6,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ke.co.openmaps.createchs.magicalkenya.data.Attraction;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class PeopleFragment extends Fragment {
 
+    @BindView(R.id.list)
+    ListView peopleList;
 
     public PeopleFragment() {
         // Required empty public constructor
@@ -23,6 +32,22 @@ public class PeopleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.default_list, container,false);
+        ButterKnife.bind(this, rootView);
+
+        // list of people
+        ArrayList<Attraction> people = new ArrayList<>(Arrays.asList(
+                new Attraction("El Molo", "Kenya's smallest tribe",
+                        "Living by the shores of Lake Turkana, they have a unique lifestyle that is quickly dying out"),
+                new Attraction("Maasai", null,
+                        "Probably Africa's best known tribe, with a vibrant culture that has survived the ages"),
+                new Attraction("Samburu", null,
+                        "A close cousin to the Maasai found in Northern Kenya with a somewhat similar culture"),
+                new Attraction("Swahili", null,
+                        "A generalization of the coastal tribes. A welcoming people with a relaxed lifestyle"),
+                new Attraction("Turkana", null,
+                        "A pastoral community found around the shores of Lake Turkana little touched by urbanisation")
+        ));
+        peopleList.setAdapter(new AttractionsAdapter(getContext(), people));
         return rootView;
     }
 
--- app/src/main/java/ke/co/openmaps/createchs/magicalkenya/PlacesFragment.java
+++ app/src/main/java/ke/co/openmaps/createchs/magicalkenya/PlacesFragment.java
@@ -30,16 +30,20 @@ public class PlacesFragment extends Fragment {
         // create list of places
         ArrayList<Attraction> places = new ArrayList<>(Arrays.asList(
                 new Attraction("Loiyangalani", null,
-                   "Explore the lake, go mountain climbing or dance with the people"),
+                   "Explore the lake, go mountain climbing or dance with the people",
+                        R.drawable.loiyangalani),
                 new Attraction("Maralal", null,
-                        "Home to the world famous camel race. The gateway to Northern Kenya"),
+                        "Home to the world famous camel race. The gateway to Northern Kenya",
+                        R.drawable.maralal),
                 new Attraction("South Horr", null,
                         "Nestled between two hills, the village offers some spectacular views",
                         R.drawable.south_horr),
                 new Attraction("Sibiloi National Park", null,
-                        "Recently accorded World Heritage status, there's plenty to see."),
+                        "Recently accorded World Heritage status, there's plenty to see.",
+                        R.drawable.sibiloi),
                 new Attraction("Turkana", "The cradle of mankind",
-                        "This place is remote, and beautiful and the journey even more so.")
+                        "This place is remote, and beautiful and the journey even more so.",
+                        R.drawable.turkana)
         ));
         // set list as arrayadapter data source
         // set adapter on the listview
